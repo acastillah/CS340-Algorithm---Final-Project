caa7caa16d1150d0341197e843f90f5f7c111f88
Connor Cassidy <cmcassidy@haverford.edu>
2016-10-19 22:32:09 -0400

    finalized integration, now we to do data extraction

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ args = parser.parse_args()
 
 
 def extract_info(class_info, student_prefs, schedule):
-    print class_info
-    print student_prefs
-    print schedule
+    print open_this(class_info)
+    print open_this(student_prefs)
+    print open_this(schedule)
     return
+
+def open_this(txt):
+    with open('basic_project_files/'+txt, 'r') as text:
+        read_data = text.read()
+        return read_data
 
 extract_info(args.class_info, args.student_prefs, args.schedule)
