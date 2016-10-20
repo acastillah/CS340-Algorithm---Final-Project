eca63fdc3a1496ec905bcb242cc5e2c9d4539e37
Connor Cassidy <cmcassidy@haverford.edu>
2016-10-20 00:45:00 -0400

    fixed error in integration logic

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ To use this codebase, simply run the following in your shell.
 ```shell
 sh run.sh demo_constraints.txt demo_studentprefs.txt demo_schedule.txt
 ```
+
+*Using:* Python 2.7
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ parser = argparse.ArgumentParser(description="Test your algortihm for the regist
 parser.add_argument('class_info')
 parser.add_argument('student_prefs')
 parser.add_argument('schedule')
+
 args = parser.parse_args()
 
 
-def extract_info(class_info, student_prefs, schedule):
-    print open_this(class_info)
-    print open_this(student_prefs)
-    print open_this(schedule)
+def extend_info(class_info, student_prefs):
+    class_info = open_this(class_info)
+    student_prefs = open_this(student_prefs)
     return
 
 def open_this(txt):
@@ -18,4 +18,5 @@ def open_this(txt):
         read_data = text.read()
         return read_data
 
-extract_info(args.class_info, args.student_prefs, args.schedule)
+extend_info(args.class_info, args.student_prefs)
+# write_schedule(args.schedule)
