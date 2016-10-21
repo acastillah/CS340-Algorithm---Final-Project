dce4b1bcf0d56a07412965bebb680ce828b26676
Alex Wang <alwang@haverford.edu>
2016-10-20 21:49:30 -0400

    Add get_possible_students() function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, operator
 
 parser = argparse.ArgumentParser(description="Test your algortihm for the registrar's problem!")
 parser.add_argument('class_txt')
@@ -8,17 +8,38 @@ parser.add_argument('schedule_txt')
 args = parser.parse_args()
 
 
-def extend_info(class_txt, student_txt):
-    extract_info(class_txt)
-    extract_info(student_txt)
-    return
+def extend_info(txt):
+    return extract_info(txt)
 
+# return a list of lists
 def extract_info(txt):
-    text = open('basic_project_files/'+txt, 'r')
+    text = iter(open('basic_project_files/'+txt, 'r'))
+    next(text) # Skip the first line
+    results = []
     for line in text:
         line = line.strip()
-        print line
+        results.append(line.split())
     text.close()
+    return results
 
-extend_info(args.class_txt, args.student_txt)
+# extend_info(args.student_txt)
 # write_schedule(args.schedule)
+
+# returns a dictionary {Class: {Students that want to take Class}
+def get_possible_students():
+    student_preference_list = extract_info(args.student_txt)
+    possible_students = {}
+
+    for student in student_preference_list:
+        student_id = student[0]
+
+        student = iter(student)
+        next(student) # skip student ID
+        for class_preference in student:
+            if class_preference in possible_students:
+                possible_students[class_preference].append(student_id)
+            else:
+                possible_students[class_preference] = [student_id]
+    return possible_students
+
+print get_possible_students()
