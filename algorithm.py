8e0aa2e802e1b1f0241271a729b0752b4a0d89b4
Adri Castilla <Adri Castilla>
2016-10-23 18:26:23 -0400

    integration with alex ds

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,15 @@
+from collections import namedtuple
+
 def registrars(ds):
     for timeslot in ds["Timeslots"]:
         classroom = ds["PossibleClassrooms"][timeslot].pop(0) # largest classroom available at timeslot
-        course = ds["PopularClasses"].pop(0) # Most popular class
+        course = ds["PopularClasses"].pop(0).id # Most popular class
         ds["Schedule"][timeslot][classroom] = {course: set()}
         #changed the way we use classroom size to go through the loop because the way you had it before you never updated it
         classroomSize = ds["ClassroomSize"][classroom]
         #changed the condition because it is not a set but a list (possiblestudents)
         while classroomSize > 0 and ds["PossibleStudents"][course]:
-            student = ds["PossibleStudents"][course].pop(0)
+            student = ds["PossibleStudents"][course].pop()
             ds["Schedule"][timeslot][classroom][course].add(student)
             ds["StudentsInTimeslot"][timeslot].add(student)
             classroomSize -= 1
@@ -17,7 +19,7 @@ def registrars(ds):
             ds["PossibleClassrooms"].pop(timeslot)
     
     while ds["PopularClasses"] and ds["PossibleClassrooms"]:
-        course = ds["PopularClasses"].pop(0)
+        course = ds["PopularClasses"].pop(0).id
         teacher = ds["ClassTeacher"][course]
         timeslotsTeacherFree = ds["Timeslots"] - ds["TeacherBusy"][teacher]
         metric = float("-inf")
@@ -44,10 +46,8 @@ def registrars(ds):
         classroomSize = ds["ClassroomSize"][classroom]
         ds["Schedule"][optimalTimeslot][classroom] = {course: set()}
         availableStudents = ds["PossibleStudents"][course] - ds["StudentsInTimeslot"][optimalTimeslot]
-        #while classroomSize > 0 and ds["PossibleStudents"][course]:
         while classroomSize > 0 and availableStudents:
-            #student = ds["PossibleStudents"][course].pop(0)
-            student = availableStudents.pop(0)
+            student = availableStudents.pop()
             ds["Schedule"][optimalTimeslot][classroom][course].add(student)
             ds["StudentsInTimeslot"][optimalTimeslot].add(student)
             classroomSize -= 1
@@ -56,14 +56,16 @@ def registrars(ds):
 
     return ds["Schedule"]
 
+PopularClass = namedtuple('PopularClass', 'id')
+
 ds = {
     "Timeslots": {1, 2},
-    "PopularClasses": ["A","D","B","C"],
+    "PopularClasses": [PopularClass(id="1"),PopularClass(id="2"),PopularClass(id="3"),PopularClass(id="4")],
 }
 
 ds["PossibleClassrooms"] = {1: [2,1,3], 2: [2,1,3]}
-ds["PossibleStudents"] = {"A": {"s1","s2","s3","s4"},"B": {"s1","s2","s4","s5","s6"},"C": {"s1","s3","s5","s6","s7"},"D": {"s2","s5","s6","s7"}}
-ds["ClassTeacher"] = {"A": "Jane","B": "Jane","C": "Alex","D": "Connor"}
+ds["PossibleStudents"] = {"1": {"s1","s2","s3","s4"},"2": {"s1","s2","s4","s5","s6"},"3": {"s1","s3","s5","s6","s7"},"4": {"s2","s5","s6","s7"}}
+ds["ClassTeacher"] = {"1": "Jane","2": "Jane","3": "Alex","4": "Connor"}
 ds["TeacherBusy"] = {"Jane": set(), "Alex": set(), "Connor": set()}
 ds["StudentsInTimeslot"] = {1: set(),2:set()}
 ds["ClassroomSize"] = {1: 6, 2: 5, 3: 8}
