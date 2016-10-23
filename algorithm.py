2cae56130cff827fc9f737d022039c21665b8792
Adri Castilla <Adri Castilla>
2016-10-23 17:07:12 -0400

    algorithm with dummy data and testing and othercorrections

--- algorithm.py
+++ algorithm.py
@@ -2,51 +2,77 @@ def registrars(ds):
     for timeslot in ds["Timeslots"]:
         classroom = ds["PossibleClassrooms"][timeslot].pop() # largest classroom available at timeslot
         course = ds["PopularClasses"].pop() # Most popular class
-        ds["Schedule"][timeslot][classroom] = {course: {}}
+        ds["Schedule"][timeslot][classroom] = {course: set()}
         #changed the way we use classroom size to go through the loop because the way you had it before you never updated it
-        classroomSize = ds["ClassroomSize"][classroom] 
-        while classroomSize > 0 or ds["PossibleStudents"][course] != set():
+        classroomSize = ds["ClassroomSize"][classroom]
+        #changed the condition because it is not a set but a list (possiblestudents)
+        while classroomSize > 0 and ds["PossibleStudents"][course]:
             student = ds["PossibleStudents"][course].pop()
             ds["Schedule"][timeslot][classroom][course].add(student)
             ds["StudentsInTimeslot"][timeslot].add(student)
             classroomSize -= 1
         teacher = ds["ClassTeacher"][course]
         ds["TeacherBusy"][teacher].add(timeslot)
-
-    while ds["PopularClasses"] != set() or ds["PossibleClassrooms"] != set():
+        if not ds["PossibleClassrooms"][timeslot]:
+            ds["PossibleClassrooms"].pop(timeslot)
+    
+    while ds["PopularClasses"] and ds["PossibleClassrooms"]:
         course = ds["PopularClasses"].pop()
         teacher = ds["ClassTeacher"][course]
         timeslotsTeacherFree = ds["Timeslots"] - ds["TeacherBusy"][teacher]
         metric = float("-inf")
         optimalMetric = float("-inf")
         optimalTimeslot = None
+        #figuring out the best timeslot to assign students and class to
         for timeslot in timeslotsTeacherFree:
-            classroom = ds["PossibleClassrooms"][timeslot].pop() # largest classroom available at timeslot
-            numOfAvailableStudents = abs(ds["PossibleStudents"][course] - ds["Students in Timeslot"][timeslot])
-            classroomSize = ds["ClassroomSize"][classroom]
+            classroom = ds["PossibleClassrooms"][timeslot][-1:]
+            numOfAvailableStudents = len(ds["PossibleStudents"][course] - ds["StudentsInTimeslot"][timeslot])
+            classroomSize = ds["ClassroomSize"][classroom.pop()]
             if classroomSize > numOfAvailableStudents:
-                metric = classroomSize
-            else:
                 metric = numOfAvailableStudents
+            else:
+                metric = classroomSize
             if metric > optimalMetric:
                 optimalTimeslot = timeslot
                 optimalMetric = metric
-        if optimalMetric == 0: # why do we have this check??
+        if optimalMetric == 0: 
             break
+        #need to check this cuz i dont think it's right as is
         classroom = ds["PossibleClassrooms"][optimalTimeslot].pop() # largest classroom available at timeslot
-        if classroom == None:
-            #ds["PossibleClassrooms"][timeslot].remove() #not sure which of these is the right way to do it
-            ds["PossibleClassrooms"].remove(timeslot)
+        if not ds["PossibleClassrooms"][optimalTimeslot]:
+            ds["PossibleClassrooms"].pop(optimalTimeslot)
         classroomSize = ds["ClassroomSize"][classroom]
-        while classroomSize > 0 or ds["PossibleStudents"][course] != set():
-            student = ds["PossibleStudents"][course].pop()
+        ds["Schedule"][optimalTimeslot][classroom] = {course: set()}
+        availableStudents = ds["PossibleStudents"][course] - ds["StudentsInTimeslot"][optimalTimeslot]
+        #while classroomSize > 0 and ds["PossibleStudents"][course]:
+        while classroomSize > 0 and availableStudents:
+            #student = ds["PossibleStudents"][course].pop()
+            student = availableStudents.pop()
             ds["Schedule"][optimalTimeslot][classroom][course].add(student)
             ds["StudentsInTimeslot"][optimalTimeslot].add(student)
             classroomSize -= 1
         teacher = ds["ClassTeacher"][course]
         ds["TeacherBusy"][teacher].add(optimalTimeslot)
+
     return ds["Schedule"]
 
+ds = {
+    "Timeslots": {1, 2},
+    "PopularClasses": ["A","D","B","C"],
+}
+
+ds["PossibleClassrooms"] = {1: [2,1,3], 2: [2,1,3]}
+ds["PossibleStudents"] = {"A": {"s1","s2","s3","s4"},"B": {"s1","s2","s4","s5","s6"},"C": {"s1","s3","s5","s6","s7"},"D": {"s2","s5","s6","s7"}}
+ds["ClassTeacher"] = {"A": "Jane","B": "Jane","C": "Alex","D": "Connor"}
+ds["TeacherBusy"] = {"Jane": set(), "Alex": set(), "Connor": set()}
+ds["StudentsInTimeslot"] = {1: set(),2:set()}
+ds["ClassroomSize"] = {1: 6, 2: 5, 3: 8}
+ds["Schedule"] = {1: {}, 2:{}}
+ds["Schedule"][1] = {1:{},2:{},3:{}}
+ds["Schedule"][2] = {1:{},2:{},3:{}}
+
+registrars(ds)
+print ds["Schedule"]
 
         
 
